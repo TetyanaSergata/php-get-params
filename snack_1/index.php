d5184e6596046435bde5f79112c262983615507a
Tetyana <tsergata@gmail.com>
2020-02-24 19:49:25 +0100

    Inizio

---
+++ snack_1/index.php
@@ -0,0 +1,17 @@
+<!-- Stampare una stringa verde se la variabile password passata in GET è uguale a “Boolean”, altrimenti stampare una stringa rossa. -->
+<?php
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Snack_1</title>
+   </head>
+   <body>
+
+   </body>
+ </html>
